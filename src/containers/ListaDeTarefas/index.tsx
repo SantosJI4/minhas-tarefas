b96c298b843ab2f14646f0f344275758bd301f7e
iury santos <iurysantos654@gmail.com>
2025-06-03 20:34:54 -0300

    construindo barra lateral

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,13 @@
+const ListaDeTarefas = () => {
+  return (
+    <main>
+      <p>2 tarefas marcadas como concluídas</p>
+      <ul>
+        <li>Tarefa 1</li>
+        <li>Tarefa 2</li>
+      </ul>
+    </main>
+  )
+}
+
+export default ListaDeTarefas
